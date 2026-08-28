e2bd85a92603f66bcd11e34eebb605fbc5d0d6e4
umerwe <uemyy1@gmail.com>
2026-08-28 12:43:03 +0500

    feat: add RoleListSkeleton and PermissionsPanelSkeleton components

--- src/components/skeletons/index.ts
+++ src/components/skeletons/index.ts
@@ -3,3 +3,5 @@ export { DashboardSkeleton } from "./dashboard-skeleton";
 export { SubscriptionPlanCardSkeleton } from "./subscription-plan-card-skeleton";
 export { SystemLogsSkeleton } from "./system-logs-skeleton";
 export { ProductDetailsSkeleton } from "./product-details-skeleton";
+export { RoleListSkeleton } from "./role-list-skeleton";
+export { PermissionsPanelSkeleton } from "./permissions-panel-skeleton";
